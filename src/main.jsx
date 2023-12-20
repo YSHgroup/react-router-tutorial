e5b216e65b8b339838d7096bb63b41357db73f0f
MagicSky <david.dev100@outlook.com>
2023-12-20 14:20:00 -0500

    Add contact route

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Root from './routes/root'
-import ErrorPage from './error-page'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Root from './routes/root';
+import ErrorPage from './error-page';
+import Contact from './routes/contact';
 // import App from './App.jsx'
-import './index.css'
+import './index.css';
 
 const router = createBrowserRouter([
-  { path: '/',
-  element: <Root/>,
-  errorElement: <ErrorPage />,
-}
-])
+	{ path: '/', element: <Root />, errorElement: <ErrorPage /> },
+	{ path: 'contacts/:contactId', element: <Contact /> },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
+);
