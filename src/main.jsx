bc80dc31e280943040046d8cb4803b878d815773
MagicSky <david.dev100@outlook.com>
2023-12-20 14:02:10 -0500

    Add Contact component and configure initial router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Root from './routes/root'
+import ErrorPage from './error-page'
+// import App from './App.jsx'
 import './index.css'
+
+const router = createBrowserRouter([
+  { path: '/',
+  element: <Root/>,
+  errorElement: <ErrorPage />,
+}
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
